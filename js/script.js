671ca6c451ac0a9df905446190ffd21f69c3bb5f
Dmitry <dvgalanin@mail.ru>
2022-09-25 22:55:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let a = 15;
+console.log(a);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const arr = [1, 2, 3];
+
+// const obj = {a: 1, b: 2}
+
+const obj = {
+    'Anna': 500,
+    'Alice': 800,
+};
+
+obj.Ivan = '700';
+obj['Ivan'] = '701';
+
+console.log(obj['Ivan']);
+console.log(obj.Ivan);
+
