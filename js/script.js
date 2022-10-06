fe4a222b900a2443fdd8d5743148d5bb9b94519f
Dmitry <dvgalanin@mail.ru>
2022-10-06 22:49:23 +0300

    06.10.2022

--- js/script.js
+++ js/script.js
@@ -1,26 +1,102 @@
 "use strict";
 
-let a = 15;
-console.log(a);
+// let result = '';
 
-let number = 5;
-const leftBorderWidth = 1;
+// first: for (let i = 1; i < 4; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 5; k++) {
+//             if (k === 3) break first;
+//             console.log(`third level: ${k}`);
+//         }
+//     }
+// }
 
-number = 10;
-console.log(number);
+// for (let i = 5; i <= 10; i++) {
+//     console.log(i);
+// }
 
-const arr = [1, 2, 3];
+// for (let i = 20; i >= 10; i--) {
+//     if (i === 13) break;
+//     console.log(i);
+// }
 
-// const obj = {a: 1, b: 2}
+// for (let i = 2; i <= 10; i++) {
+//     if (! (i % 2)) {
+//         console.log(i);
+//     }
+// }
 
-const obj = {
-    'Anna': 500,
-    'Alice': 800,
-};
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
 
-obj.Ivan = '700';
-obj['Ivan'] = '701';
+// let i = 2;
 
-console.log(obj['Ivan']);
-console.log(obj.Ivan);
+// while (i >= 2 && i <= 16) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }    
+//     i++;
+// } 
 
+// const arrayOfNumbers = [];
+// let x = 5;
+// let y = 10;
+
+// for (let i = x; i <= y; i++) {
+//     arrayOfNumbers[i - x] = i;
+// }
+
+// console.log(arrayOfNumbers);
+
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
+
+// console.log(result);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// for (let i = 0; i < data.length; i++) {
+//     if (typeof(data[i]) === 'number') {
+//         data[i] = data[i] * 2;
+//     } else {
+//         data[i] = data[i] += " - done";
+//     }    
+// }
+
+// console.log(data);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// let result = [];
+
+// for (let i = 0; i < data.length; i++) {
+//     result[i] = data[data.length - (i + 1)];
+// }
+
+// console.log(result);
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i < lines + 1; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";        
+    }
+    for (let k = 0 ; k <= i * 2; k++) {
+        result += "*";        
+    }   
+    result += "\n";
+}
+
+console.log(result);
